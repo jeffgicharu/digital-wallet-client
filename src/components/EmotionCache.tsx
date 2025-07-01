f4c3821c4177fc4e8dd11554d9377b0eb21aa4bc
jeffgicharu <jeff.kaharu@students.jkuat.ac.ke>
2025-07-01 21:13:19 +0300

    fix: resolve explicit-any type error in EmotionCache

--- src/components/EmotionCache.tsx
+++ src/components/EmotionCache.tsx
@@ -3,12 +3,22 @@
 import * as React from 'react';
 import { useServerInsertedHTML } from 'next/navigation';
 import { CacheProvider } from '@emotion/react';
-import createCache from '@emotion/cache';
+import createCache, { EmotionCache } from '@emotion/cache';
 
-export default function NextAppDirEmotionCacheProvider(props: any) {
+interface EmotionCacheProviderProps {
+  options: {
+    key: string;
+  };
+  children: React.ReactNode;
+}
+
+export default function NextAppDirEmotionCacheProvider(props: EmotionCacheProviderProps) {
   const { options, children } = props;
 
-  const [{ cache, flush }] = React.useState(() => {
+  const [{ cache, flush }] = React.useState<{
+    cache: EmotionCache;
+    flush: () => string[];
+  }>(() => {
     const cache = createCache(options);
     cache.compat = true;
     const prevInsert = cache.insert;
