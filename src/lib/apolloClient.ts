e7c7bf2c64d1cf352f21f4d1aa79be6f470826d5
jeffgicharu <jeff.kaharu@students.jkuat.ac.ke>
2025-07-04 16:40:05 +0300

    feat: Update Apollo Client to send auth token

--- src/lib/apolloClient.ts
+++ src/lib/apolloClient.ts
@@ -1,9 +1,26 @@
-import { ApolloClient, InMemoryCache, HttpLink } from "@apollo/client";
+import { ApolloClient, InMemoryCache, HttpLink, from } from "@apollo/client";
+import { setContext } from '@apollo/client/link/context';
 
+const httpLink = new HttpLink({
+  uri: "http://localhost:8080/graphql", // The backend endpoint
+});
+
+// This authLink middleware will add the token to the headers
+const authLink = setContext((_, { headers }) => {
+  // Get the authentication token from local storage if it exists
+  const token = localStorage.getItem('authToken');
+  // Return the headers to the context so httpLink can read them
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : "",
+    }
+  }
+});
+
+// Chain the authLink and httpLink together
 const client = new ApolloClient({
-  link: new HttpLink({
-    uri: "http://localhost:8080/graphql", // The backend endpoint from the project brief 
-  }),
+  link: from([authLink, httpLink]),
   cache: new InMemoryCache(),
 });
 
